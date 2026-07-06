c999569cd58f7bfa5c7e391e831cba402c1c6e3f
atulcodes-tech <subhkaranjaiswal41@gmail.com>
2026-07-06 15:35:10 +0530

    Done

---
+++ Linked List/SingularLinkedList/LinkedList.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
+
+function insertAtFirst(head, x){
+    let newNode = new Node(x);
+    newNode.next = head;
+    return newNode;
+}
+
+function print(head){
+
+    let temp = head;
+
+    while(temp !== null){
+        process.stdout.write.toString(temp.data)
+        if(temp.next !== null){
+            process.stdout.write(" -> ")
+        }
+        temp = temp.next;
+    }
+    console.log();
+
+}
+function main(){
+
+    let list = new LinkedList();
+    list.head = insertAtFirst(list.head, 1);
+    list.head = insertAtFirst(list.head, 1);
+    list.head = insertAtFirst(list.head, 1);
+    list.head = insertAtFirst(list.head, 1);
+    list.head = insertAtFirst(list.head, 1);
+
+    
+
+    print(list)
+
+}
+main();
